c41f7ca2dcaed0890763c7a2df13d8f82dd1509d
gastoneberle <gastoneberleezequiel@gmail.com>
2019-05-13 12:27:14 -0300

    CD

--- Practica/Practica/CD.cs
+++ Practica/Practica/CD.cs
@@ -6,48 +6,12 @@ using System.Threading.Tasks;
 
 namespace Practico4
 {
-    class Medios
+    class CD:Medio
     {
-        //Atributos
-        private string titulo;
-        private string interprete;
-        protected int duracion;
+
 
         //Constructor
-        public Medios(string titulo, string interprete, int duracion)
-        {
-            this.titulo = titulo;
-            this.interprete = interprete;
-            this.duracion = duracion;
-        }
+        public CD(string titulo, string interprete, int duracion):base(titulo,interprete,duracion){ }
 
-        //Get Set
-        private string Titulo1 { get => titulo; set => titulo = value; }
-        public string Interprete { get => interprete; set => interprete = value; }
-                
-        public int Duracion
-        {
-            get
-            {
-                return duracion;
-            }
-            set
-            {
-                duracion = value;
-            }
-        }
-
-        public string Titulo {
-            get
-            {
-                return titulo;
-            }
-            set
-            {
-                titulo = value;
-            }
-        }
-
-        
     }
 }
