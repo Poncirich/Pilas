1ec631d23d9278b118a4ef3382410d8a9ecfb00c
gastoneberle <gastoneberleezequiel@gmail.com>
2019-05-13 11:49:58 -0300

    new class cd and medio

---
+++ Practica/Practica/Medio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico4
+{
+    class Medio
+    {
+        //Atributos
+        protected string titulo;
+        private string interprete;
+        protected int duracion;
+
+        //Constructor
+        public Medio(string titulo, string interprete, int duracion)
+        {
+            this.titulo = titulo;
+            this.interprete = interprete;
+            this.duracion = duracion;
+        }
+
+        //Get Set
+        public string Interprete { get => interprete; set => interprete = value; }
+
+        //Metodos
+        public int Duracion
+        {
+            get
+            {
+                return duracion;
+            }
+            set
+            {
+                duracion = value;
+            }
+        }
+
+        public string Titulo {
+            get
+            {
+                return titulo;
+            }
+            set
+            {
+                titulo = value;
+            }
+        }
+    }
+}
