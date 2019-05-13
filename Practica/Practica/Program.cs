1b2f8ec76dcecd26b56ce04de5fb3f42f65c6920
Pablo Poder <pablo.poder.mail@gmail.com>
2019-05-13 10:34:22 -0300

    Cambio 1

--- Practica/Practica/Program.cs
+++ Practica/Practica/Program.cs
@@ -10,6 +10,7 @@ namespace Practico4
     {
         static void Main(string[] args)
         {
+            //hola maurito
             Pila pi = new Pila(5);
             Pila pi1 = new Pila(5);
             Pila aux = new Pila(5);
